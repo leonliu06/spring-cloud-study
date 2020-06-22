2f5f51223b8d89a76d5458a54d134c5fc7708375
leonliu06 <leonliu06@gmail.com>
2020-06-22 16:28:28 +0800

    分布式配置中心组件spring cloud config客户端配置

--- consul-demo-consumer/src/main/java/com/mrliuli/consuldemo/ConsulDemoConsumerApplication.java
+++ consul-demo-consumer/src/main/java/com/mrliuli/consuldemo/ConsulDemoConsumerApplication.java
@@ -1,13 +1,18 @@
 package com.mrliuli.consuldemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
+@RestController
 public class ConsulDemoConsumerApplication {
 
     @Bean
@@ -18,6 +23,14 @@ public class ConsulDemoConsumerApplication {
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(ConsulDemoConsumerApplication.class, args);
         System.out.println(applicationContext.getEnvironment().getProperty("secretKey"));
+    }
+
+    @Value("${foo}")
+    private String foo;
+
+    @RequestMapping("hi")
+    public String hi() {
+        return foo;
     }
 
 }
