f99b421527b8317a23ccebac30f0c54563896a32
leonliu06 <leonliu06@gmail.com>
2020-06-23 14:03:14 +0800

    spring cloud config 客户端自动刷新获取服务端配置

--- config-client/src/main/java/com/mrliuli/config/ConfigClientApplication.java
+++ config-client/src/main/java/com/mrliuli/config/ConfigClientApplication.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 //@EnableDiscoveryClient
 @SpringBootApplication
-@RefreshScope
+@RefreshScope   // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
 @RestController
 public class ConfigClientApplication {
 
